571255c5c5334f5604dda4b2efeba45c2685d0c0
jack-margeson <margeson.jack@gmail.com>
2017-11-05 09:48:02 -0500

    Fixed grammar

--- name.js
+++ name.js
@@ -27,7 +27,7 @@ function setnamebad() {
 
 	document.getElementById("hide").style.display = "none";
 	
-	document.getElementById('name').innerHTML = ('Ugh. Its\xa0' +name+'.')
+	document.getElementById('name').innerHTML = ('Ugh. It\'s\xa0' +name+'.')
 	
 	var adjectives = ['aggressive','aloof','arrogant','belligerent','big-headed','boastful','bone-idle','boring','bossy','callous','cantankerous','careless','changeable','clinging','compulsive','conservative','cowardly','cruel','cunning','cynical','deceitful','detached','dishonest','dogmatic','domineering','finicky','flirtatious','foolish','foolhardy','fussy','greedy','grumpy','gullible','harsh','impatient','impolite','impulsive','inconsiderate','inconsistent','indecisive','indiscreet','inflexible','interfering','intolerant','irresponsible','jealous','lazy','Machiavellian','materialistic','mean','miserly','moody','narrow-minded','nasty','naughty','nervous','obsessive','obstinate','overcritical','overemotional','parsimonious','patronizing','perverse','pessimistic','pompous','possessive','pusillanimous','quarrelsome','quick-tempered','resentful','rude','ruthless','sarcastic','secretive','selfish','self-centred','self-indulgent','silly','sneaky','stingy','stubborn','stupid','superficial','tactless','timid','touchy','thoughtless','truculent','unkind','unpredictable','unreliable','untidy','untrustworthy','vague','vain','vengeful','vulgar','weak-willed']
 	var random = adjectives[Math.floor(Math.random() * adjectives.length)];
