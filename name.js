cc337727ab7f607e3e86d5400975aec89e54a6a9
jack-margeson <margeson.jack@gmail.com>
2017-11-05 06:33:29 -0500

    Overall project direction changed, compliment generator created

--- name.js
+++ name.js
@@ -6,7 +6,7 @@ function stopRKey(evt) {
 	} 
 	document.onkeypress = stopRKey; 
 
-// oof
+// sets function for name and displays adjective
 function setname() {
 	var name
 	name = document.getElementById('userinput').value;
@@ -14,5 +14,10 @@ function setname() {
 	document.getElementById("hide").style.display = "none";
 	
 	document.getElementById('name').innerHTML = ('Hello,\xa0' +name+'!')
+	
+	var adjectives = ['adaptable','adventurous','affable','affectionate','agreeable','ambitious','amiable','amicable','amusing','brave','bright','broad-minded','calm','careful','charming','communicative','compassionate','conscientious','considerate','convivial','courageous','courteous','creative','decisive','determined','diligent','diplomatic','discreet','dynamic','easygoing','emotional','energetic','enthusiastic','exuberant','fair-minded','faithful','fearless','forceful','frank','friendly','funny','generous','gentle','good','gregarious','hard-working','helpful','honest','humorous','imaginative','impartial','independent','intellectual','intelligent','intuitive','inventive','kind','loving','loyal','modest','neat','nice','optimistic','passionate','patient','persistent','pioneering','philosophical','placid','plucky','polite','powerful','practical','pro-active','quick-witted','quiet','rational','reliable','reserved','resourceful','romantic','self-confident','self-disciplined','sensible','sensitive','shy','sincere','sociable','straightforward','sympathetic','thoughtful','tidy','tough','unassuming','understanding','versatile','warmhearted','willing','witty'];  
+	var random = adjectives[Math.floor(Math.random() * adjectives.length)];
+	
+	document.getElementById('adjective').innerHTML = ('You are a very\xa0' +random+'\xa0person.')
 }
-	// alert(name+'-setname')
+	
